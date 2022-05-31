b9cc4232fc322836b19fd8ee14e4858e7437b931
Monu Bhagat <52962847+monu17569@users.noreply.github.com>
2022-05-31 18:09:23 +0530

    Updates

---
+++ precache-manifest.616ab508cf3a4197e7325f895f4add5e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b61b6b88eb5d2fa23490",
+    "url": "/textutils/static/css/main.f4f25c12.chunk.css"
+  },
+  {
+    "revision": "b61b6b88eb5d2fa23490",
+    "url": "/textutils/static/js/main.b61b6b88.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "5b977f20bbc5700a25d3",
+    "url": "/textutils/static/js/2.5b977f20.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "5f1bbd09079708a37e1ce160330163b5",
+    "url": "/textutils/index.html"
+  }
+];
